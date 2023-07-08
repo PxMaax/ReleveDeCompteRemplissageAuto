c748a5efca77fb4454da409ab667d3c94bd39948
PxMaax <maxoujacquot@gmail.com>
2023-07-08 22:40:04 +0200

    Sa

--- ProgrammeReleveDeCompte.py
+++ ProgrammeReleveDeCompte.py
@@ -1,11 +1,58 @@
 import tkinter as tk
 from tkinter import font
 from tkinter import ttk
+import openpyxl as pyxl
+import glob
+import csv
+import os
+from tkinter import filedialog
+
+WindowWidth = "600"
+WindowHeight = "500"
+windowResolution = WindowWidth + "x" + WindowHeight
 
 
-def DonnerFeuilleDeCompta():
-    # Logique de la fonction DonnerFeuilleDeCompta
-    print("Donner feuille de compta")
+# def DonnerFeuilleDeCompta():
+#     FileC = filedialog.askopenfilename(initialdir="Desktop/", title="Feuille de compta")
+#     print(FileC)
+#     if ".xlsx" in FileC:
+#         if FileC != "":
+#             window.canva1.create_oval(360, 60, 390, 90, fill="green")
+#             # canva1.create_oval(
+#             #     releve_btn.winfo_x(),
+#             #     releve_btn.winfo_y(),
+#             #     releve_btn.winfo_x() + 40,
+#             #     releve_btn.winfo_y() + 40,
+#             #     fill="green",
+#             # )
+#             return FileC
+#         else:
+#             window.tk.create_oval(360, 60, 390, 90, fill="red")
+#             return ""
+#     else:
+#         window.AffichageMessage(
+#             "ERREUR: Extension fichier non reconnue. \n Je pense que tu es juste pas douée et que tu t'es juste trompé de fichier\n Sinon appelle Maxime il va regler le probleme"
+#         )
+#         window.tk.create_oval(360, 60, 390, 90, fill="red")
+#         return ""
+
+# Logique de la fonction DonnerFeuilleDeCompta
+
+
+print("Donner feuille de compta")
+
+
+## FONCTION EN RAPPORT AVEC L'INTERFACE GRAPHIQUE ##
+## FONCTION EN RAPPORT AVEC L'INTERFACE GRAPHIQUE ##
+## FONCTION EN RAPPORT AVEC L'INTERFACE GRAPHIQUE ##
+## FONCTION EN RAPPORT AVEC L'INTERFACE GRAPHIQUE ##
+## FONCTION EN RAPPORT AVEC L'INTERFACE GRAPHIQUE ##
+## FONCTION EN RAPPORT AVEC L'INTERFACE GRAPHIQUE ##
+
+# Couleurs
+bg_color = "#55868C"
+button_color = "#7F636E"
+text_color = "#000000"
 
 
 def DonnerReleveDeCompte():
@@ -29,14 +76,10 @@ def LancerProgramme():
 # Création de la fenêtre principale
 window = tk.Tk()
 window.title("Relevé de compte automatique")
-window.geometry("600x500")  # Taille fixe de la fenêtre
-window.configure(bg="#55868C")  # Couleur de fond de la fenêtre
+window.geometry(windowResolution)  # Taille fixe de la fenêtre
+window.configure(bg=bg_color)  # Couleur de fond de la fenêtre
 window.resizable(False, False)  # Désactiver le redimensionnement de la fenêtre
 
-# Couleurs
-bg_color = "#55868C"
-button_color = "#7F636E"
-text_color = "#000000"
 
 # Police
 header_font = font.Font(family="Baskerville", size=20, weight="bold")
@@ -45,6 +88,12 @@ button_font = font.Font(family="Baskerville", size=14, weight="bold")
 
 # Espacement
 y_spacing = 40  # Espacement vertical entre les éléments
+x_spacing = -10
+
+
+# window.canva1 = tk.Canvas(
+#     window, bg=bg_color, width=WindowWidth, height=WindowHeight
+# ).place(x=0, y=0)
 
 # Entête
 header_label = tk.Label(
@@ -72,7 +121,7 @@ feuille_btn = tk.Button(
     bg=button_color,
     fg=text_color,
 )
-feuille_btn.place(x=50, y=80 + y_spacing, width=250)
+feuille_btn.place(x=50 + x_spacing, y=80 + y_spacing, width=250)
 
 # Bouton Donner relevé de compte
 releve_btn = tk.Button(
@@ -83,8 +132,7 @@ releve_btn = tk.Button(
     bg=button_color,
     fg=text_color,
 )
-releve_btn.place(x=50, y=130 + y_spacing, width=250)
-
+releve_btn.place(x=50 + x_spacing, y=130 + y_spacing, width=250)
 # Entrée pour le mois et l'année
 selection_label = tk.Label(
     window,
@@ -93,7 +141,7 @@ selection_label = tk.Label(
     bg=bg_color,
     fg=text_color,
 )
-selection_label.place(x=50, y=180 + y_spacing * 2)
+selection_label.place(x=50 + x_spacing, y=180 + y_spacing)
 
 # Menu déroulant pour le mois
 month_var = tk.StringVar(window)
@@ -120,7 +168,7 @@ month_dropdown.config(
     font=button_font, bg=button_color, fg=text_color, highlightthickness=0
 )
 
-month_dropdown.place(x=50, y=220 + y_spacing * 3, width=250)
+month_dropdown.place(x=50 + x_spacing, y=220 + y_spacing, width=250)
 
 # Menu déroulant pour l'année
 year_var = tk.StringVar(window)
@@ -129,9 +177,9 @@ year_dropdown = tk.OptionMenu(window, year_var, "2023", "2024", "2025")
 year_dropdown.config(
     font=button_font, bg=button_color, fg=text_color, highlightthickness=0
 )
-year_dropdown.place(x=320, y=220 + y_spacing * 3, width=150)
+year_dropdown.place(x=320 + x_spacing, y=220 + y_spacing, width=150)
 
-# Bouton Soumettre le mois et l'année
+# Bouton Soumettre le mois et l'annéereleve_btn
 submit_btn = tk.Button(
     window,
     text="Soumettre",
@@ -140,7 +188,7 @@ submit_btn = tk.Button(
     bg=button_color,
     fg=text_color,
 )
-submit_btn.place(x=490, y=220 + y_spacing * 3, width=100)
+submit_btn.place(x=490 + x_spacing, y=220 + y_spacing, width=100)
 
 # Affichage du mois et de l'année sélectionnés
 selected_label = tk.Label(
@@ -150,17 +198,17 @@ selected_label = tk.Label(
     bg=bg_color,
     fg=text_color,
 )
-selected_label.place(x=50, y=280 + y_spacing * 4)
+selected_label.place(x=50 + x_spacing, y=280 + y_spacing)
 
 selected_month_label = tk.Label(
     window, textvariable=month_var, font=label_font, bg=bg_color, fg=text_color
 )
-selected_month_label.place(x=50, y=320 + y_spacing * 5)
+selected_month_label.place(x=50, y=320 + y_spacing)
 
 selected_year_label = tk.Label(
     window, textvariable=year_var, font=label_font, bg=bg_color, fg=text_color
 )
-selected_year_label.place(x=180, y=320 + y_spacing * 5)
+selected_year_label.place(x=180, y=320 + y_spacing)
 
 # Bouton Lancer le programme
 launch_btn = tk.Button(
@@ -171,7 +219,7 @@ launch_btn = tk.Button(
     bg=button_color,
     fg=text_color,
 )
-launch_btn.place(x=20, y=450 + y_spacing * 7, width=250)
+launch_btn.place(x=20, y=450, width=250)
 
 # Bouton Fermer la page
 close_btn = tk.Button(
@@ -182,7 +230,8 @@ close_btn = tk.Button(
     bg=button_color,
     fg=text_color,
 )
-close_btn.place(x=430, y=450 + y_spacing * 7, width=150)
+close_btn.place(x=430, y=450, width=150)
+
 
 # Lancement de la boucle principale
 window.mainloop()
